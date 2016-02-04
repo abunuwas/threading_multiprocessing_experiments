2a0267cf4a788cd32d7eb830eb15eb50ef12b0ae
abunuwas <joseharoperalta@gmail.com>
2016-02-04 19:22:57 +0100

    Add non-rigorous function to find all words in a text

--- text_analysis.py
+++ text_analysis.py
@@ -3,12 +3,19 @@ from os import listdir
 from os.path import isfile, join
 
 
+def list_texts(path):
+    return [join(path, f) for f in listdir(path)
+            if isfile(join(path, f)) and f.endswith('.txt')]
+
 def open_file(file):
     return open(file, 'r', encoding='utf-8').read()
+
+
+
+def get_words(text):
+    return re.findall('\w+', text)
 
 def find_pattern(pattern, text):
     text = open_file(text)
     return re.findall(pattern, text)
 
-def list_texts(path):
-    return [join(path, f) for f in listdir(path) if isfile(join(path, f)) and f.endswith('.txt')]
