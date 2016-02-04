8ea07ba71ab19364d5f2f81df946f75f6f5186e6
abunuwas <joseharoperalta@gmail.com>
2016-02-04 17:28:55 +0100

    Create module to compare the performance of threading vs. multiprocessing

---
+++ threads_vs_processes.py
@@ -0,0 +1,13 @@
+import timeit
+
+process_time = timeit.timeit('run_processes()',
+                             setup='from processes import run_processes',
+                             number=1)
+threading_time = timeit.timeit('run_threads()',
+                               setup='from threads import run_threads",
+                               number=1)
+
+print("""
+Time to analyze texts with threads: {}
+Time to analyze texts with multiprocessing: {}
+""".format(threading_time, process_time))
