3c3bf00d540d23dea1690e967c15cbf56ed47655
abunuwas <joseharoperalta@gmail.com>
2016-02-04 17:38:46 +0100

    Wrap threading and multiprocessing functions within a __name__ == '__main__' conditional statement

--- threads_vs_processes.py
+++ threads_vs_processes.py
@@ -1,13 +1,16 @@
-import timeit
 
-process_time = timeit.timeit('run_processes()',
-                             setup='from processes import run_processes',
-                             number=1)
-threading_time = timeit.timeit('run_threads()',
-                               setup='from threads import run_threads",
-                               number=1)
+if __name__ == "__main__":
 
-print("""
-Time to analyze texts with threads: {}
-Time to analyze texts with multiprocessing: {}
-""".format(threading_time, process_time))
+    import timeit
+
+    process_time = timeit.timeit('run_processes()',
+                                 setup='from processes import run_processes',
+                                 number=1)
+    threading_time = timeit.timeit('run_threads()',
+                                   setup="from threads import run_threads",
+                                   number=1)
+
+    print("""
+    Time to analyze texts with threads: {}
+    Time to analyze texts with multiprocessing: {}
+    """.format(threading_time, process_time))
