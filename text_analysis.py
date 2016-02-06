ea59242c9854c80c5eb0f14dd548ab64fbaf38bb
abunuwas <joseharoperalta@gmail.com>
2016-02-07 00:08:46 +0100

    Add function to build a generator with all files in a directory

--- text_analysis.py
+++ text_analysis.py
@@ -7,6 +7,10 @@ def list_texts(path):
     return [join(path, f) for f in listdir(path)
             if isfile(join(path, f)) and f.endswith('.txt')]
 
+def gen_texts(path):
+    return (join(path, f) for f in listdir(path)
+            if isfile(join(path, f)) and f.endswith('.txt'))
+
 def open_file(file):
     return open(file, 'r', encoding='utf-8').read()
 
